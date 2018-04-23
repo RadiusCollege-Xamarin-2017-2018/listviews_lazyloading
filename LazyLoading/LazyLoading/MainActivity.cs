d4daf673d64b9e8851ed17511ce6838978b63419
Unknown <d233407@edu.rocwb.nl>
2018-04-23 13:13:15 +0200

    Lazyloading first commit

--- LazyLoading/LazyLoading/MainActivity.cs
+++ LazyLoading/LazyLoading/MainActivity.cs
@@ -13,6 +13,26 @@ namespace LazyLoading
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            LinearLayout LazyLoadingContainer = FindViewById<LinearLayout>(Resource.Id.LazyloadingContainer);
+            ScrollView LazyScroller = new ScrollView(this);
+            LinearLayout ScrollContainer = new LinearLayout(this);
+            System.Lazy<string> items = new System.Lazy<string>();
+                
+            LazyLoadingContainer.AddView(LazyScroller);
+            ScrollContainer.Orientation = Orientation.Vertical;
+            LazyScroller.AddView(ScrollContainer);
+            
+            for (int i = 0; i < 100; i++)
+            {
+                
+
+
+                TextView text = new TextView(this);
+                text.SetText(i.ToString(), TextView.BufferType.Normal);
+
+                ScrollContainer.AddView(text);
+            }
         }
     }
 }
